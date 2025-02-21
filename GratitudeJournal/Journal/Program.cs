560c71c67f07f898a0058fff3c3273f430220ead
Kelly Lougheed <kellylougheed@gmail.com>
2025-02-21 02:13:37 +0000

    Basic functionality to add entry

--- GratitudeJournal/Journal/Program.cs
+++ GratitudeJournal/Journal/Program.cs
@@ -1,7 +1,73 @@
 ﻿namespace Journal;
 
+using Spectre.Console;
+using System.IO;
+using System;
+using System.Globalization;
+
 class Program {
     static void Main(string[] args) {
-        Console.WriteLine("Hello, world!");
+
+        Console.WriteLine("Welcome to your Gratitude Journal!");
+
+        var selection = AnsiConsole.Prompt(
+            new SelectionPrompt<string>()
+                .AddChoices(new[] {
+                    "Add Entry", "View Entries", "Get Ideas", 
+                }));
+
+        if (selection == "Add Entry") {
+            AddEntry();
+        } else if (selection == "View Entries") {
+            ViewEntries();
+        } else if (selection == "Get Ideas") {
+            GetIdeas();
+        }
+    }
+
+    static void AddEntry() {
+
+        DateTime date = DateTime.Today;
+        Console.WriteLine("Gratitude Entry for " + date.ToString(new CultureInfo("en-us")));
+
+        string addSelection = "Add Another";
+
+        List<string> items = new List<string>();
+
+        while (addSelection == "Add Another") {
+
+            Console.WriteLine("What are you grateful for?");
+            string item = Console.ReadLine();
+
+            if (item.Length > 0) {
+                items.Add(item);
+            }
+            
+            addSelection = AnsiConsole.Prompt(
+            new SelectionPrompt<string>()
+                .AddChoices(new[] {
+                    "Add Another", "Save and Exit", 
+                }));
+        }
+        if (addSelection == "Save and Exit") {
+
+            // TODO: SAVE DATE + LIST TO A FILE
+
+            Console.WriteLine("Your list:");
+            Console.WriteLine(items);
+            Console.WriteLine("Exiting now");
+        }
+        
+
+        
+    }
+
+     static void ViewEntries() {
+
+    }
+
+     static void GetIdeas() {
+
+
     }
 }
